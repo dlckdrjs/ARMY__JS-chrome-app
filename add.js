2273625ff4d16b714c276d8eece1643997f3f15c
dlckdrjs <cklee0518@naver.com>
2022-08-28 22:54:22 +0900

    4.1 Form Sunmission

--- add.js
+++ add.js
@@ -2,7 +2,13 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function handleBtnClick() {
-  console.log(loginInput.value);
+  const userName = loginInput.value;
+
+  if (userName === "") {
+    alert("Please write your name!");
+  } else if (userName.length > 15) {
+    alert("Your name is too long.");
+  }
 }
 
 loginButton.addEventListener("click", handleBtnClick);
