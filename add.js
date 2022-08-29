1e165cb462fb981790633101145e84a0111e9cd2
dlckdrjs <cklee0518@naver.com>
2022-08-29 19:23:37 +0900

    4.4 getting Username

--- add.js
+++ add.js
@@ -1,9 +1,13 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
 function handleBtnClick(event) {
   event.preventDefault();
-  console.log(loginInput.value);
+  loginForm.classList.add("hidden");
+  const username = loginInput.value;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove("hidden");
 }
 
 loginForm.addEventListener("submit", handleBtnClick);
