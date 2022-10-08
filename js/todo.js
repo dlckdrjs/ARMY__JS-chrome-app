2d6cdf727d1a8f2da40106a61440d01c765d3a19
dlckdrjs <cklee0518@naver.com>
2022-10-08 23:35:44 +0900

    study TO DO LIST JS - 7.6 Deleting To Dos part One ~ 7.7 Deleting To Dos part Two ~ 7.8 Deleting To Dos part Three

--- js/todo.js
+++ js/todo.js
@@ -2,35 +2,35 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
-const TODOS_KEY = "todos"
+const TODOS_KEY = "todos";
 
-let toDos = []
+let toDos = [];
 
-function saveToDos(){
-  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos))
-  
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
-function deleteToDo(event){
+function deleteToDo(event) {
   const li = event.target.parentElement;
-  li.remove()
+  li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
 
 function paintToDo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
-  span.innerText = newTodo;
-  const button = document.createElement("button")
-  button.innerText = "❌"
+  span.innerText = newTodo.text;
+  const button = document.createElement("button");
+  button.innerText = "❌";
 
-  button.addEventListener("click", deleteToDo)
-  
+  button.addEventListener("click", deleteToDo);
+
   li.appendChild(span);
   li.appendChild(button);
-  
-  
+
   toDoList.appendChild(li);
- 
 }
 
 function handleToDoSubmit(event) {
@@ -38,20 +38,22 @@ function handleToDoSubmit(event) {
   const newTodo = toDoInput.value;
   toDoInput.value = "";
 
-  toDos.push(newTodo)
-  paintToDo(newTodo);
-  saveToDos()
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
+const savedToDos = localStorage.getItem(TODOS_KEY);
 
-
-const savedToDos = localStorage.getItem(TODOS_KEY)
-
-if(saveToDos !==null){
- const parsedToDos = JSON.parse(savedToDos)
- toDos = parsedToDos
- parsedToDos.forEach(paintToDo)
- 
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
 }
