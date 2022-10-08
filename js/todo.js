ea96ed2ce2e7c8abfb81fc9e227a983d527c4bd2
dlckdrjs <cklee0518@naver.com>
2022-10-08 15:11:09 +0900

    studyTO DO LIST JS - 7.4 Loading To Dos part One

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,15 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos"
+
+const toDos = []
+
+function saveToDos(){
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos))
+  
+}
+
 function deleteToDo(event){
   const li = event.target.parentElement;
   li.remove()
@@ -13,12 +22,15 @@ function paintToDo(newTodo) {
   span.innerText = newTodo;
   const button = document.createElement("button")
   button.innerText = "❌"
+
   button.addEventListener("click", deleteToDo)
   
   li.appendChild(span);
   li.appendChild(button);
   
+  
   toDoList.appendChild(li);
+ 
 }
 
 function handleToDoSubmit(event) {
@@ -26,7 +38,18 @@ function handleToDoSubmit(event) {
   const newTodo = toDoInput.value;
   toDoInput.value = "";
 
+  toDos.push(newTodo)
   paintToDo(newTodo);
+  saveToDos()
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+
+
+const savedToDos = localStorage.getItem(TODOS_KEY)
+
+if(saveToDos !==null){
+ const parsedToDos = JSON.parse(savedToDos)
+ parsedToDos.forEach((item)=>console.log("This is the",item))
+}
